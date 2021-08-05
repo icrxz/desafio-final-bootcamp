2259868319546de0ff691646b6b671980c14c64f
mafachin <marina.mfachin@mercadolivre.com>
2021-08-05 19:10:16 -0300

    refact: update product type

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/enums/ProductTypeEnum.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/enums/ProductTypeEnum.java
@@ -1,9 +1,7 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.entities.enums;
 
 public enum ProductTypeEnum {
-    CARNES,
-    CONGELADOS,
-    EMBUTIDOS,
-    LATICÍNIOS,
-    FRUTAS
+    FS,
+    RF,
+    FF
 }
