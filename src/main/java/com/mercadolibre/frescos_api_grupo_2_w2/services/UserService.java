16328d7e46aa931eabc6921c7a2f5d736073ce0e
Waschington Luiz Menezes Filho <waschington.menezes@mercadolivre.com>
2021-08-06 14:18:40 -0300

    feat: add token authentication

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/services/UserService.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/services/UserService.java
@@ -6,13 +6,20 @@ import com.mercadolibre.frescos_api_grupo_2_w2.dtos.UserDTO;
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.Seller;
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.Supervisor;
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.User;
+import com.mercadolibre.frescos_api_grupo_2_w2.exceptions.UserAlreadyExists;
 import com.mercadolibre.frescos_api_grupo_2_w2.repositories.UserRepository;
+import com.mercadolibre.frescos_api_grupo_2_w2.security.DetailUserData;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Autowired
@@ -31,6 +38,21 @@ public class UserService {
         } else {
             // TODO exception
         }
+
+        Optional<User> user = this.userRepository.findByEmail(userDTO.getEmail());
+        if (!user.isEmpty()) {
+            throw new UserAlreadyExists("Este email já está cadastrado");
+        }
         return this.userRepository.save(newUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(s);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Usuário [" + s + "] não encontrado");
+        }
+
+        return new DetailUserData(user);
     }
 }
