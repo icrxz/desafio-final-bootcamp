e57e79f6764dcc0444e621626bf53a685ba328bf
icrxz <igor.cruz@mercadolivre.com>
2021-08-11 16:30:04 -0300

    feature: Add repository methods to find Batches by product

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/repositories/BatchRepository.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/repositories/BatchRepository.java
@@ -1,9 +1,16 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.repositories;
 
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.Batch;
+import com.mercadolibre.frescos_api_grupo_2_w2.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface BatchRepository extends JpaRepository<Batch, Long> {
+    List<Batch> findBatchesByProduct(Product product);
+
+    List<Batch> findBatchesByProduct_productId(UUID productId);
 }
