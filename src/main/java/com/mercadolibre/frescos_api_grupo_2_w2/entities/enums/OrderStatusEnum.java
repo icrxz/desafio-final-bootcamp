4888e9dea59c7940e78ec6d2e9b2071f496c8114
icrxz <igor.cruz@mercadolivre.com>
2021-08-11 16:20:18 -0300

    refactor: update enums structure

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/enums/OrderStatusEnum.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/enums/OrderStatusEnum.java
@@ -1,8 +1,30 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.entities.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum OrderStatusEnum {
-    CONFIRMANDO,
-    SEPARANDO,
-    TRANSPORTE,
-    ENTREGUE
+    CONFIRMING(0, "Confirmando"),
+    SORTING(1, "Separando"),
+    TRANSPORT(2, "Transporte"),
+    DELIVERED(3, "Entregue");
+
+    private final Integer code;
+    private final String description;
+
+    public static OrderStatusEnum toEnum(Integer code) {
+        if (code == null) {
+            return null;
+        }
+
+        for (OrderStatusEnum orderStatus : OrderStatusEnum.values()) {
+            if (code.equals(orderStatus.getCode())) {
+                return orderStatus;
+            }
+        }
+
+        throw new IllegalArgumentException("Order Status not valid: " + code);
+    }
 }
--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/enums/ProductTypeEnum.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/enums/ProductTypeEnum.java
@@ -1,7 +1,29 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.entities.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ProductTypeEnum {
-    FS,
-    RF,
-    FF
+    FRESH("FS", "Fresco"),
+    REFRIGERATED("RF", "Refrigerado"),
+    FROZEN("FF", "Congelado");
+
+    private final String code;
+    private final String description;
+
+    public static ProductTypeEnum toEnum(String code) {
+        if (code == null) {
+            return null;
+        }
+
+        for (ProductTypeEnum productType : ProductTypeEnum.values()) {
+            if (code.equals(productType.getCode())) {
+                return productType;
+            }
+        }
+
+        throw new IllegalArgumentException("Product type not valid: " + code);
+    }
 }
