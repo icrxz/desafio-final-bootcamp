47cde86305c96f830ba246005bbc9172dc131b94
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-08-11 09:39:46 -0300

    feat: create exception ProductNotFoundException

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/exceptions/ProductNotFoundException.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/exceptions/ProductNotFoundException.java
@@ -1,2 +1,10 @@
-package com.mercadolibre.frescos_api_grupo_2_w2.exceptions;public class ProductNotFoundException {
+package com.mercadolibre.frescos_api_grupo_2_w2.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+
 }
