a8532941d420077c02d222a5f41852e16db7f797
icrxz <igor.cruz@mercadolivre.com>
2021-08-11 16:30:19 -0300

    feature: Add repository methods to find Product by type

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/repositories/ProductRepository.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/repositories/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.repositories;
 
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.Product;
+import com.mercadolibre.frescos_api_grupo_2_w2.entities.enums.ProductTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findProductByType(ProductTypeEnum productType);
 }
