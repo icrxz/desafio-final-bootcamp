f0abfc250220174ddaed44039c11a890f61e0a6e
icrxz <igor.cruz@mercadolivre.com>
2021-08-11 16:19:11 -0300

    refactor: delete Cart entity and update Buyer

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/Buyer.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/Buyer.java
@@ -1,20 +1,22 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.entities;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
+@EqualsAndHashCode(callSuper = true)
 public class Buyer extends User{
     private String cpf;
 
-    @OneToOne
-    private Cart cart;
-
-    @OneToMany(mappedBy = "buyer")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "buyer")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private List<PurchaseOrder> purchaseOrders;
 }
