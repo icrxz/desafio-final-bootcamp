1cc39ed191adaa0db087223bcc3db30ff697a5ec
icrxz <igor.cruz@mercadolivre.com>
2021-08-11 16:36:09 -0300

    feature: add PurchaseOrder relation with PurchaseOrderProducts

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/PurchaseOrder.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/entities/PurchaseOrder.java
@@ -1,13 +1,23 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.entities;
 
+import com.mercadolibre.frescos_api_grupo_2_w2.entities.enums.OrderStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PurchaseOrder{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,6 +28,7 @@ public class PurchaseOrder{
     @ManyToOne
     private Buyer buyer;
 
-    @ManyToMany
-    private List<Product> products;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "purchaseOrder")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<PurchaseOrderProduct> product;
 }
