5665788b4f2cb3350ef28839e26c13391b5ab962
icrxz <igor.cruz@mercadolivre.com>
2021-08-13 02:07:28 -0300

    refactor: update users mocks

--- src/test/java/com/mercadolibre/frescos_api_grupo_2_w2/util/mocks/UserSellerMock.java
+++ src/test/java/com/mercadolibre/frescos_api_grupo_2_w2/util/mocks/UserSellerMock.java
@@ -3,22 +3,37 @@ package com.mercadolibre.frescos_api_grupo_2_w2.util.mocks;
 import com.mercadolibre.frescos_api_grupo_2_w2.dtos.forms.user.SellerForm;
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.Seller;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 public class UserSellerMock {
-    public static Seller validSeller (Optional<Long> id) {
+    public static Long userId = 1L;
+    private static PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public static Seller validSeller (Long id) {
         Seller sellerMock = new Seller();
-        sellerMock.setEmail("any_email@email.com");
-        sellerMock.setPassword("any_password");
+        sellerMock.setEmail("seller@email.com");
+        sellerMock.setPassword(encoder.encode("123"));
         sellerMock.setCnpj("any_cnpj");
         sellerMock.setRole("SELLER");
         sellerMock.setProducts(new ArrayList<>());
+        sellerMock.setUserId(id);
 
-        if (id != null) {
-            sellerMock.setUserId(id.get());
-        }
+        return sellerMock;
+    }
+
+    public static Seller validSeller () {
+        Seller sellerMock = new Seller();
+        sellerMock.setEmail("seller@email.com");
+        sellerMock.setPassword(encoder.encode("123"));
+        sellerMock.setCnpj("any_cnpj");
+        sellerMock.setRole("SELLER");
+        sellerMock.setProducts(new ArrayList<>());
+        sellerMock.setUserId(userId);
+
         return sellerMock;
     }
 
