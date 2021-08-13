c8993a3b4d5ef48686c45078068f6eb69691e497
icrxz <igor.cruz@mercadolivre.com>
2021-08-13 02:00:47 -0300

    refactor: add first supervisor using repository

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/Application.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/Application.java
@@ -2,6 +2,10 @@ package com.mercadolibre.frescos_api_grupo_2_w2;
 
 import com.mercadolibre.frescos_api_grupo_2_w2.config.SpringConfig;
 import com.mercadolibre.frescos_api_grupo_2_w2.dtos.forms.user.SupervisorForm;
+import com.mercadolibre.frescos_api_grupo_2_w2.entities.Supervisor;
+import com.mercadolibre.frescos_api_grupo_2_w2.repositories.SellerRepository;
+import com.mercadolibre.frescos_api_grupo_2_w2.repositories.SupervisorRepository;
+import com.mercadolibre.frescos_api_grupo_2_w2.repositories.UserRepository;
 import com.mercadolibre.frescos_api_grupo_2_w2.services.UserService;
 import com.mercadolibre.frescos_api_grupo_2_w2.util.ScopeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +17,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 public class Application {
-	private final UserService userService;
+	private final SupervisorRepository supervisorRepository;
 
 	@Autowired
-	public Application(UserService userService) {
-		this.userService = userService;
+	public Application(SupervisorRepository supervisorRepository) {
+		this.supervisorRepository = supervisorRepository;
 
-		if (userService.getUsers().size() <= 0)
+		if (supervisorRepository.findAll().size() <= 0)
 			createAdminUser();
 	}
 
 	private void createAdminUser() {
-		SupervisorForm supervisor = new SupervisorForm();
+		Supervisor supervisor = new Supervisor();
 		supervisor.setEmail("admin@admin.com");
 		supervisor.setPassword("$2a$10$JHzsz1ZmVNNYtxiiaSlyl.M8rwfyPPfePg4FRUkPmKMM4T3c2/cFi");
+		supervisor.setRole("SUPERVISOR");
 
-		userService.createUser(supervisor);
+		supervisorRepository.save(supervisor);
 	}
+
 	public static void main(String[] args) {
 		ScopeUtils.calculateScopeSuffix();
 		new SpringApplicationBuilder(SpringConfig.class).registerShutdownHook(true).run(args);
