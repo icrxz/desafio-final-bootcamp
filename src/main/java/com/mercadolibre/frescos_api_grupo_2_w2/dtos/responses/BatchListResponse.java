55940ee3c10ec88248a35170f2b030da87d1a13b
Jonathan <jonathan.cavalcanti@mercadolivre.com>
2021-08-13 10:39:40 -0300

    feat: create BatchListResponse

---
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/dtos/responses/BatchListResponse.java
@@ -0,0 +1,20 @@
+package com.mercadolibre.frescos_api_grupo_2_w2.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchListResponse {
+    private long batchNumber;
+    private int currentQuantity;
+    private LocalDate dueDate;
+}
