933f07b2aec1ddef1c7a71f473e131793486e2af
icrxz <igor.cruz@mercadolivre.com>
2021-08-16 08:27:47 -0300

    feature: find buyer by email

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/repositories/BuyerRepository.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/repositories/BuyerRepository.java
@@ -1,9 +1,13 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.repositories;
 
 import com.mercadolibre.frescos_api_grupo_2_w2.entities.Buyer;
+import com.mercadolibre.frescos_api_grupo_2_w2.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BuyerRepository extends JpaRepository<Buyer, Long> {
+    Optional<Buyer> findByEmail(String email);
 }
