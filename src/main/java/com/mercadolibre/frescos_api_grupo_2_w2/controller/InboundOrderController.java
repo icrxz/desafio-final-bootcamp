fbf1bc03e4639eb57f9d92c413d3320f3eb74f63
icrxz <igor.cruz@mercadolivre.com>
2021-08-15 23:58:54 -0300

    feature: add endpoint to update inbound order

--- src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/controller/InboundOrderController.java
+++ src/main/java/com/mercadolibre/frescos_api_grupo_2_w2/controller/InboundOrderController.java
@@ -1,7 +1,9 @@
 package com.mercadolibre.frescos_api_grupo_2_w2.controller;
 
 import com.mercadolibre.frescos_api_grupo_2_w2.dtos.forms.InboundOrder.InboundOrderForm;
+import com.mercadolibre.frescos_api_grupo_2_w2.dtos.forms.purchaseOrder.PurchaseOrderForm;
 import com.mercadolibre.frescos_api_grupo_2_w2.dtos.responses.InboundOrderResponse;
+import com.mercadolibre.frescos_api_grupo_2_w2.dtos.responses.purchaseOrder.PurchaseOrderResponse;
 import com.mercadolibre.frescos_api_grupo_2_w2.services.InboundOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/inboundorder")
@@ -25,5 +28,15 @@ public class InboundOrderController {
         InboundOrderResponse newInboundOrder = inboundOrderService.createInboundOrder(inboundOrderForm);
 
         return new ResponseEntity<>(newInboundOrder, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<InboundOrderResponse> updateInboundOrder(
+            @RequestParam Long orderId,
+            @RequestBody @Valid InboundOrderForm inboundOrderForm
+    ) {
+        InboundOrderResponse inboundOrder = inboundOrderService.updateInboundOrder(orderId, inboundOrderForm);
+
+        return new ResponseEntity<>(inboundOrder, HttpStatus.OK);
     }
 }
